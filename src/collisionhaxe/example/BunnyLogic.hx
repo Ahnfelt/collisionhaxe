6a3d46d546cf026cd793c07e4e7c21bddd9dc6c1
werk <mfwerk@gmail.com>
2015-06-17 19:01:02 +0200

    moving bunny

--- src/collisionhaxe/example/BunnyLogic.hx
+++ src/collisionhaxe/example/BunnyLogic.hx
@@ -9,15 +9,18 @@ class BunnyLogic {
     public function new() {}
 
     var bunnySprite1 : Sprite;
-    var bunny1 = new Actor(new BoundingBox(100, 100, 10, 10));
+    var bunny1 = new Actor(new BoundingBox(100, 100, 40, 40), 1, 0);
+    var grid = new SparseGrid<Actor>(200, 200);
 
     public function init(stage : Container) {
-        bunnySprite1 = new Sprite(Texture.fromImage("assets/bunny.png"));
+        bunnySprite1 = new Sprite(Texture.fromImage("../assets/bunny.png"));
         bunnySprite1.anchor.set(0.5, 0.5);
         stage.addChild(bunnySprite1);
+        grid.insert(bunny1.boundingBox, bunny1);
     }
 
     public function update(elapsedTime : Float, stage : Container) {
+        bunny1.move(grid, elapsedTime);
         bunnySprite1.position.set(bunny1.boundingBox.x, bunny1.boundingBox.y);
    	}
 
